e876d3ea5b54937569ba12e04dbd8edc79792139
UongHoangAnh45 <88814845+BanhMat45@users.noreply.github.com>
2021-10-06 07:09:52 +0700

    UHA033

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UongHoangAnh033.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
