09e8f46fbaed48c4cd7a37d4127a7eea0d0c157d
UongHoangAnh45 <88814845+BanhMat45@users.noreply.github.com>
2021-10-06 08:11:27 +0700

    Final_Commit _UHA033

---
+++ Models/StringProcess.cs
@@ -0,0 +1,10 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UongHoangAnh033.Models
+{
+    public class StringProcess
+    {
+        public string  StringProcessUHA033 { get; set; }
+    }
+}
